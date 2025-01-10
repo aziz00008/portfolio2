9d208d0f9369db5fdce482eb50deeee8d09e130e
aziz00008 <keskesaziz77@gmail.com>
2025-01-10 17:41:03 +0100

    f

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        {/* Other routes */}
+      </Switch>
+    </Router>,
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
